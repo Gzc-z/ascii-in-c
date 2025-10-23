76802a2bce9011de8bca6dd29c4b84a3b5018124
Gabriel Zaiac <gz.gabrielzaiac@gmail.com>
2025-10-23 01:13:28 -0300

    learning: understanding c :|

--- tas.c
+++ tas.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  char string[100];
-  scanf("%s", string);
-  printf("%s world!\n", string);
+  FILE *file;
+  file = fopen("test.txt", "r");
+
+  char text[100];
+
+  while (fgets(text, 100, file)) {
+    printf("%s", text);
+  }
+  printf("%s", file);
+
+  fclose(file);
   return 0;
 }
